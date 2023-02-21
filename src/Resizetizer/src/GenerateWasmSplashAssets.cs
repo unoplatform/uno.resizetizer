b4e0148d013f5e2332a67909c6d613f776147387
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-02-21 15:35:26 -0300

    feat: created the Splash task for wasm

---
+++ src/Resizetizer/src/GenerateWasmSplashAssets.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Build.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uno.Resizetizer;
+public class GenerateWasmSplashAssets : Task
+{
+	public override bool Execute()
+	{
+		throw new NotImplementedException();
+	}
+}
