69dfca7044fe8dd5208c238fa1e56f21e867feff
Pedro Jesus <pedrojesus.cefet@gmail.com>
2023-02-24 17:02:53 -0300

    fix: changed the way js file is handled

--- src/Resizetizer/src/GenerateWasmSplashAssets.cs
+++ src/Resizetizer/src/GenerateWasmSplashAssets.cs
@@ -2,8 +2,12 @@
 using Microsoft.Build.Utilities;
 using SkiaSharp;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Uno.Resizetizer;
 public class GenerateWasmSplashAssets : Task
@@ -73,42 +77,84 @@ public class GenerateWasmSplashAssets : Task
 	void ProcessAppManifestFile(in string appManifestPath, ResizeImageInfo info, StreamWriter writer)
 	{
 		using var reader = new StreamReader(File.OpenRead(appManifestPath));
+		var fileToProcess = reader.ReadToEnd();
 
-		while (!reader.EndOfStream)
-		{
-			var line = reader.ReadLine();
-			if (line.TrimStart().StartsWith("splashScreenImage:"))
-				line = ProcessSplashScreenImageLine(line, info);
-			else if (line.TrimStart().StartsWith("splashScreenColor:"))
-				line = ProcessSplashScreenColor(line, info);
+		var dic = FindWhatINeed(fileToProcess);
 
-			writer.WriteLine(line);
-		}
+		dic["splashScreenImage"] = $"{info.OutputName}.scale-400.png";
+		dic["splashScreenColor"] = ProcessSplashScreenColor(info);
+
+		WriteToFile(dic, writer);
 	}
 
-	static string ProcessSplashScreenColor(string line, ResizeImageInfo info)
+	static void WriteToFile(Dictionary<string, string> dic, StreamWriter writer)
+	{
+		var sb = new StringBuilder(@"var UnoAppManifest = {").AppendLine();
+		foreach (var kvp in dic)
+		{
+			var key = kvp.Key;
+			var value = kvp.Value;
+			sb.AppendLine($"    {key}: \"{value}\",");
+		}
+		sb.Append('}');
+
+		var final = sb.ToString();
+
+		writer.Write(final);
+	}
+
+	static Dictionary<string, string> FindWhatINeed(string fileToProcess)
+	{
+		var indexOfSymbol = fileToProcess.IndexOf('{');
+		var input = fileToProcess.Substring(indexOfSymbol);
+		// Remove whitespace and newlines from the input
+		input = input.Replace(" ", "").Replace("\r", "").Replace("\n", "");
+
+		// Remove the opening and closing braces from the input
+		input = input.TrimStart('{').TrimEnd('}');
+
+		var dictionary = new Dictionary<string, string>();
+
+		// Split the input into key-value pairs
+		var pairs = input.Split(',');
+
+		foreach (var pair in pairs)
+		{
+			// Split each key-value pair into its components
+			var components = pair.Split(':');
+
+			// Remove the quotes from the key and value
+			var key = components[0].Replace("\"", "");
+			var value = components[1].Replace("\"", "");
+
+			// Add the key-value pair to the dictionary
+			dictionary.Add(key, value);
+		}
+
+		// Print the dictionary
+		foreach (var pair in dictionary)
+		{
+			Console.WriteLine($"{pair.Key}: {pair.Value}");
+		}
+
+		return dictionary;
+	}
+
+	static string ProcessSplashScreenColor(ResizeImageInfo info)
 	{
 		var color = ColorWithoutAlpha(info.Color);
-		var newLine = $"    splashScreenColor: \"{color}\",";
-		return line.Replace(line, newLine);
-
-
-		// Wasm doesn't support alpha
-		static string ColorWithoutAlpha(SKColor? color)
-		{
-			var result = color?.ToString() ?? "transparent";
-			if (!result.StartsWith("#"))
-				return result;
-
-			// Getting everything after '#ff'
-			result = result.Substring(3);
-			return "#" + result;
-		}
+		return $"{color}";
 	}
 
-	static string ProcessSplashScreenImageLine(in string line, ResizeImageInfo info)
+	// Wasm doesn't support alpha
+	static string ColorWithoutAlpha(SKColor? color)
 	{
-		var newLine = $"    splashScreenImage: \"{info.OutputName}.scale-400.png\",";
-		return line.Replace(line, newLine);
+		var result = color?.ToString() ?? "transparent";
+		if (!result.StartsWith("#"))
+			return result;
+
+		// Getting everything after '#ff'
+		result = result.Substring(3);
+		return "#" + result;
 	}
 }
